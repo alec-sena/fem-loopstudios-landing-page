45404c6f9a1475bde574c58af44de538c5fb5d51
alec-sena <alexanderwsena@gmail.com>
2021-10-19 13:57:14 -0400

    refactor nav

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const navHamburger = document.getElementById("nav-hamburger");
 const navClose = document.getElementById("nav-close");
-const landing = document.getElementById("landing");
-const landingNavLinks = document.getElementById("landing__nav-links");
+/* const landing = document.getElementById("landing");
+const landingNavLinks = document.getElementById("landing__nav-links"); */
+const navLinks = document.getElementById("js-navLinksMobile");
 
 const toggleNavMenu = () => {
     navHamburger.classList.toggle("display-none");
     navClose.classList.toggle("display-none");
-    landing.classList.toggle("display-none");
-    landingNavLinks.classList.toggle("display-none");
+    navLinks.classList.toggle("display-none");
+    //landing.classList.toggle("display-none");
+    //landingNavLinks.classList.toggle("display-none");
 }
 
 navHamburger.addEventListener("click", toggleNavMenu);
